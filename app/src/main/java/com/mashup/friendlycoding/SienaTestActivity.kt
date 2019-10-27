8db94b59b4e64a5df056ef892ed100682c21ca13
YuChocopie <rladbwjd341@naver.com>
2019-10-27 17:05:39 +0900

    issue 6

--- app/src/main/java/com/mashup/friendlycoding/SienaTestActivity.kt
+++ app/src/main/java/com/mashup/friendlycoding/SienaTestActivity.kt
@@ -1,23 +1,40 @@
 package com.mashup.friendlycoding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.mashup.friendlycoding.databinding.ActivitySienaTestBinding
 import com.mashup.friendlycoding.viewmodel.SienaTestViewModel
 
+
 class SienaTestActivity : AppCompatActivity() {
 
     private var mSienaTestViewModel = SienaTestViewModel()
+    lateinit var layoutMainView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivitySienaTestBinding>(this, R.layout.activity_siena_test)
+        val binding = DataBindingUtil.setContentView<ActivitySienaTestBinding>(
+            this,
+            R.layout.activity_siena_test
+        )
+        layoutMainView = this.findViewById(R.id.mainLayout)
         binding.lifecycleOwner = this
         binding.testSiena = mSienaTestViewModel
         mSienaTestViewModel.setPrincessImage(binding.ivPrinsecess)
 
 
     }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+
+        Log.e("Layout Width - ", "Width" + (layoutMainView.width))
+        Log.e("Layout Height - ", "height" + (layoutMainView.height))
+
+        mSienaTestViewModel.setViewSize(layoutMainView.width, layoutMainView.height)
+
+    }
+
 }
--- app/src/main/java/com/mashup/friendlycoding/viewmodel/SienaTestViewModel.kt
+++ app/src/main/java/com/mashup/friendlycoding/viewmodel/SienaTestViewModel.kt
@@ -1,5 +1,6 @@
 package com.mashup.friendlycoding.viewmodel
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.lifecycle.ViewModel
 
@@ -9,9 +10,16 @@ class SienaTestViewModel : ViewModel() {
     var princessImg: ImageView? = null
     var xy = true
     var unit = 1
+    var width = 0;
+    var height = 0;
 
     fun setPrincessImage(view: ImageView) {
         this.princessImg = view
+    }
+
+    fun setViewSize(width: Int, height: Int) {
+        this.width = width
+        this.height = height
     }
 
 
@@ -27,16 +35,18 @@ class SienaTestViewModel : ViewModel() {
         xy = !xy
         unit = if (!xy) -unit else unit
     }
-    fun changeX(){
-        for (i in 0..50)
-        princessImg!!.x = (princessImg!!.x + 4*unit).toFloat()
+
+    fun changeX() {
+        princessImg!!.x = (princessImg!!.x + width/10 * unit)
         Thread.sleep(100)
+        Log.e("123123", "" + princessImg!!.x)
 
     }
-    fun changeY(){
-        for (i in 0..50)
-            princessImg!!.y = (princessImg!!.y + 4*unit).toFloat()
+
+    fun changeY() {
+        princessImg!!.y = (princessImg!!.y + height/10 * unit)
         Thread.sleep(100)
+        Log.e("123123", "" + princessImg!!.y)
 
 
     }
